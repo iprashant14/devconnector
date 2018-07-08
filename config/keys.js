01e25fe797b6447eecbcb83ede47eaa0b583e013
iprashant14 <iprashantpandey@gmail.com>
2018-07-08 13:30:47 +0530

    initialisation using express and setting of routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://ipp14:12345678pp@ds127851.mlab.com:27851/devconnector"
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express();
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to mongodb
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World</h1>");
+});
+
+//Routes
+
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT : ${port}`);
+});
